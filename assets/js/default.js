f14a1c3ca6aa2798b3eeb5e4085f085fc4e58e5d
Mirela Pulbere <mipulbere@gmail.com>
2018-09-04 19:29:06 -0400

    created a modal that appears when you click on an image

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,5 +1,3 @@
-
-
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -10,3 +8,31 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 }
+
+function handleImgClick (event) {
+  var img = document.getElementById('dialogImage');
+  img.src = "https://instagram.com/p/" + event.currentTarget.id + "/media/?size=l";
+  
+  var modal = document.getElementById('myModal');
+  modal.style.display = "block";
+
+}
+
+function registerEvents () {
+  var modal = document.getElementById('myModal');
+
+  var spans = document.getElementsByClassName("close");
+  var span = spans[0];
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+
+  // todo:
+  // register handle image click here
+}
+
