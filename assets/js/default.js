25131c4139fb09e2d4f2eed81bd1aad1b41edb39
Alex Pulbere <paccodun@gmail.com>
2018-11-28 22:50:53 -0500

    use query instead of static

--- assets/js/default.js
+++ assets/js/default.js
@@ -61,47 +61,77 @@ function registerEvents () {
           modal.style.display = "none";
       }
   }
-
-  // todo:
-  // register handle image click here
 }
 
 var artData;
 var queriedData;
-var picId =[];
 var area =[];
 var imageTakenDate =[];
 
-function getData () {
-  axios.get('https://firestore.googleapis.com/v1beta1/projects/street-art-ottawa/databases/(default)/documents/artCollection')
+function queryAll () {
+  return axios.get('https://firestore.googleapis.com/v1beta1/projects/street-art-ottawa/databases/(default)/documents/artCollection')
   .then(function(response){
-    console.log(response.data); 
-    console.log(response.status); 
-    artData = response.data;
+    return response.data;
   }); 
-  return artData;
 }
 
-function showData (artData) {
-  var imgCollectionDiv = document.getElementById('imgCollection');
-  for(var i in artData){
-    var element = artData[i]
-    for(key in element){
-      var picData = element[key];
-      for(subKey in picData){
-        if(subKey == "fields"){
-          var finaldata = picData[subKey];
-          picId = finaldata.id;
-          var res = picId.stringValue;
-          var artImage = document.createElement("img");
-          artImage.src = "https://instagram.com/p/" + res + "/media/?size=l";
-          imgCollectionDiv.appendChild(artImage);
-          console.log(res);
+function displayImages (pageId) {
+  
+  var promise;
+
+  if(pageId === "art") {
+    promise = queryAll();
+  } else {
+    promise = queryLatestSix();
+  }
+
+  promise.then(function(artData) {
+    var imgCollectionDiv = document.getElementById('imgCollection');
+    for(var i in artData){
+      var element = artData[i]
+      for(key in element){
+        var picData = element[key];
+        for(subKey in picData){
+          if(subKey == "fields"){
+            var imgId = picData[subKey].id.stringValue;
+            var liElem = document.createElement("li");
+            liElem.setAttribute("class", "landscape imageContainer");
+            liElem.onclick = function(event){ handleImgClick(event)};
+            liElem.id=imgId;
+            var artImage = document.createElement("img");
+            artImage.src = "https://instagram.com/p/" + imgId + "/media/?size=l";
+            liElem.appendChild(artImage);
+            imgCollectionDiv.appendChild(liElem);
+          }
         }
       }
     }
-  }
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
 }
+
+function queryLatestSix () {
+  return axios.post('https://firestore.googleapis.com/v1beta1/projects/street-art-ottawa/databases/(default)/documents:runQuery', 
+  { 
+    "structuredQuery": { 
+      "orderBy": [{
+            "field": {
+                "fieldPath": "imageTakenDate"
+            },
+            "direction":"ASCENDING"
+            
+        }],
+        "from": [ { "collectionId": "artCollection" } ],
+        "limit":6
+      } 
+    })
+  .then(function (response) {
+    return response.data;
+  });
+}
+
 
 
 function query () {
@@ -126,27 +156,6 @@ function query () {
   .catch(function (error) {
     console.log(error);
   });
-}
-
-function populateHomePage (queriedData) {
-  var imgCollectionDiv = document.getElementById('imgCollection');
-  for(var i in queriedData){
-    var element = queriedData[i]
-    for(key in element){
-      var picData = element[key];
-      for(subKey in picData){
-        if(subKey == "fields"){
-          var finaldata = picData[subKey];
-          picId = finaldata.id;
-          var res = picId.stringValue;
-          var artImage = document.createElement("img");
-          artImage.src = "https://instagram.com/p/" + res + "/media/?size=l";
-          imgCollectionDiv.appendChild(artImage);
-          console.log(res);
-        }
-      }
-    }
-  }
 }
 
 
