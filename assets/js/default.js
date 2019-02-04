e1bccef0b310bafb4ea97c302060190db8a892bc
pulbere <pulbere@users.noreply.github.com>
2019-02-03 21:21:23 -0500

    clean up (#41)
    * clean up
    
    * change design a bit
    
    * fix dialog a bit

--- assets/js/default.js
+++ assets/js/default.js
@@ -16,12 +16,6 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
-
-function handleImgClick (event) {
-  var img = document.getElementById('dialogImage');
-  img.src = "https://instagram.com/p/" + event.currentTarget.id + "/media/?size=l";
-}
-
 function handleSelectorClick (event) {
   var area = event.currentTarget.dataset;
   var query;
@@ -36,14 +30,30 @@ function handleSelectorClick (event) {
 function getFilterQuery(filter) {
  return { 
     "structuredQuery": { 
-      "where": { 
-        "fieldFilter": { 
-          "field": { "fieldPath": "area" }, 
-          "op": "EQUAL", 
-          "value": { 
-            "stringValue": filter } 
-          } 
-        }, 
+      "where": {
+        "compositeFilter": {
+          "op": "AND",
+          "filters": [
+            {
+              "fieldFilter": {
+                "field": { "fieldPath": "area" }, 
+                "op": "EQUAL", 
+                "value": { 
+                  "stringValue": filter
+                }
+              }
+            },{
+              "fieldFilter": { 
+                "field": { "fieldPath": "exists" }, 
+                "op": "EQUAL", 
+                "value": { 
+                  "booleanValue": true
+                }
+              }
+            }
+          ]
+        } 
+      }, 
         "from": [ { "collectionId": "artCollection" } ] 
       } 
     };
@@ -61,9 +71,9 @@ function displayImages (pageId) {
 
 
 class ModalDialog extends React.Component {
-  constructor(){
-    super();
-    this.state = {isOpen: false, currentImageId: null, description: null};
+  constructor(props){
+    super(props);
+    this.state = {isOpen: true, currentImageId: props.id, description: props.description};
   }
   openModal(artId, description) {
     this.setState({isOpen: true, currentImageId: artId, description: description});
@@ -99,7 +109,7 @@ const ModalContent = ({ handleClose, isOpen, currentImageId, description}) => {
   return React.createElement('div', {style: {display: displayValue}, className:"reactModal"},
     React.createElement('div', {className:"topSection"}, 
     React.createElement('div', {className:"leftSection"}, 
-    React.createElement('img', {className: "modalImage", src: "https://www.instagram.com/p/" + currentImageId + "/media/?size=m"})), 
+    React.createElement('img', {className: "modalImage", src: "https://www.instagram.com/p/" + currentImageId + "/media/?size=l"})), 
     React.createElement('div', {className:"rightSection"}, 
     React.createElement('button', {onClick: handleClose, className: "closeButton"}, "Close"), 
     React.createElement('p', {className:"imgDescription"}, description ))),
@@ -107,9 +117,9 @@ const ModalContent = ({ handleClose, isOpen, currentImageId, description}) => {
   );
 };
 
-function createModal() {
+function createModal(id, description) {
   var modalDiv = document.getElementById("modal")
-  return ReactDOM.render(React.createElement(ModalDialog), modalDiv);
+  return ReactDOM.render(React.createElement(ModalDialog, {id: id, description: description}), modalDiv);
 }
 
 
@@ -118,16 +128,17 @@ function handleImgClick (event) {
 }
 function showModal(id, description, lat, lng) {
   if(!modalDialog) {
-    modalDialog = createModal();
+    modalDialog = createModal(id, description);
+  } else {
+    modalDialog.openModal(id, description);
   }
-  modalDialog.openModal(id, description);
   initMap(parseFloat(lat), parseFloat(lng));
 }
 
 class Art extends React.Component {
   render() {
       return React.createElement('li', {id: this.props.id, 
-      'data-long': this.props.coordinates.longitude,
+      'data-lng': this.props.coordinates.longitude,
       'data-lat': this.props.coordinates.latitude,
       'data-description': this.props.description,
        className: "landscape imageContainer", onClick: function(event){ handleImgClick(event)}},
@@ -177,9 +188,18 @@ function queryLatestSix () {
             "field": {
                 "fieldPath": "imageTakenDate"
             },
-            "direction":"ASCENDING"
+            "direction":"DESCENDING"
             
         }],
+        "where": {
+          "fieldFilter": { 
+            "field": { "fieldPath": "exists" }, 
+            "op": "EQUAL", 
+            "value": { 
+              "booleanValue": true
+            }
+          }
+        },
         "from": [ { "collectionId": "artCollection" } ],
         "limit":6
       } 
@@ -193,12 +213,21 @@ function queryAll () {
 
 function getAllQuery() {
   return { 
-    "structuredQuery": { 
+    "structuredQuery": {
+      "where": {
+        "fieldFilter": { 
+          "field": { "fieldPath": "exists" }, 
+          "op": "EQUAL", 
+          "value": { 
+            "booleanValue": true
+          }
+        }
+      },
       "orderBy": [{
             "field": {
                 "fieldPath": "imageTakenDate"
             },
-            "direction":"ASCENDING"
+            "direction":"DESCENDING"
             
         }],
         "from": [ { "collectionId": "artCollection" } ]
@@ -217,88 +246,67 @@ function execQuery(query) {
   });
 }
 
-function queryX() {
-  axios.post('https://firestore.googleapis.com/v1beta1/projects/street-art-ottawa/databases/(default)/documents:runQuery', 
-  { 
-    "structuredQuery": { 
-      "where": { 
-        "fieldFilter": { 
-          "field": { "fieldPath": "area" }, 
-          "op": "EQUAL", 
-          "value": { 
-            "stringValue": "Downtown" } 
-          } 
-        }, 
-        "from": [ { "collectionId": "artCollection" } ] 
-      } 
-    })
-  .then(function (response) {
-    console.log(response.data);
-    queriedData = response.data;
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-}
-
-
 function initMapOfAllArt() {
-  var ottawaCenter = {lat: 45.410006, lng: -75.714202}; 
+  var mapElement = document.getElementById('mapOfAllArt');
+  if (mapElement) {
+    var ottawaCenter = {lat: 45.410006, lng: -75.714202}; 
 
-  var map = new google.maps.Map(document.getElementById('mapOfAllArt'), {
-    zoom: 12,
-    maxZoom: 16,
-    center: ottawaCenter,
-    gestureHandling: 'cooperative',
-    disableDefaultUI: true, 
-    styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
-  });
+    var map = new google.maps.Map(document.getElementById('mapOfAllArt'), {
+      zoom: 12,
+      maxZoom: 18,
+      center: ottawaCenter,
+      gestureHandling: 'cooperative',
+      disableDefaultUI: true, 
+      styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
+    });
 
-  queryAll().then(function(artData) {
-    var markers =[];
-    artData.map(element => {
-      if(element.document && element.document.fields && element.document.fields.id) {
-        if (element.document.fields.coordinates) {
-          // key: element.document.fields.id.stringValue, 
-          // id : element.document.fields.id.stringValue, 
-          var coords = {
-            lat:  parseFloat(element.document.fields.coordinates.geoPointValue.latitude), 
-            lng:  parseFloat(element.document.fields.coordinates.geoPointValue.longitude)
-          };
-          var marker = new google.maps.Marker({
-            position: coords,
-            title: 'Art location',
-            map: map,
-            icon : {
-              path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
-              scale: 6,
-              fillColor: 'gray',
-              fillOpacity: 1,
-              strokeWeight: 2,
-              strokeColor: 'black'
-            }
-          });
-          marker.setValues({id:element.document.fields.id.stringValue, description:element.document.fields.description?element.document.fields.description.stringValue:null});
-          google.maps.event.addListener(marker, "click", function(event) {
-            event.cancelBubble = !0,
-            event.stopPropagation && event.stopPropagation(),
-            showModal(this.id, this.description, event.latLng.lat(), event.latLng.lng());
-        }),
-          markers.push(marker);
+    queryAll().then(function(artData) {
+      var markers =[];
+      artData.map(element => {
+        if(element.document && element.document.fields && element.document.fields.id) {
+          if (element.document.fields.coordinates) {
+            var coords = {
+              lat:  parseFloat(element.document.fields.coordinates.geoPointValue.latitude), 
+              lng:  parseFloat(element.document.fields.coordinates.geoPointValue.longitude)
+            };
+            var marker = new google.maps.Marker({
+              position: coords,
+              title: 'Art location',
+              map: map,
+              icon : {
+                path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+                scale: 6,
+                fillColor: 'gray',
+                fillOpacity: 1,
+                strokeWeight: 2,
+                strokeColor: 'black'
+              }
+            });
+            marker.setValues({id:element.document.fields.id.stringValue, description:element.document.fields.description?element.document.fields.description.stringValue:null});
+            google.maps.event.addListener(marker, "click", function(event) {
+              event.cancelBubble = !0,
+              event.stopPropagation && event.stopPropagation(),
+              showModal(this.id, this.description, event.latLng.lat(), event.latLng.lng());
+          }),
+            markers.push(marker);
+          }
         }
-      }
-    })
-    var markerClusterOptions = {
-      maxZoom:16,
-
-      styles:[{"anchorIcon":[0,0],"anchorText":[0,0],"fontFamily":"inherit","fontWeight":"normal","height":46,"textColor":"black","textSize":20,
-        "url":"assets/circle.svg",
-        "width":46}]
-      };
-    
-    var markerCluster = new MarkerClusterer(map, markers, markerClusterOptions);
-  })
+      })
+      var markerClusterOptions = {
+        gridSize: 17,
+        maxZoom: 18,
+        styles:[{"anchorIcon":[0,0],"anchorText":[0,0],"fontFamily":"inherit","fontWeight":"normal","height":46,"textColor":"black","textSize":20,
+          "url":"assets/circle.svg",
+          "width":46}]
+        };
+      
+      var markerCluster = new MarkerClusterer(map, markers, markerClusterOptions);
+    });
+  }
 }
+
+var dialogMap;
+var dialogMarker;
 
 function initMap(lat, lng) {
   var eternalFlame = {lat: 45.423730, lng: -75.698700};  
@@ -307,15 +315,22 @@ function initMap(lat, lng) {
     artCoords = eternalFlame;
   }
 
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 16,
-    center: artCoords,
-    gestureHandling: 'cooperative'
-  });
+  if (!dialogMap) {
+    dialogMap = new google.maps.Map(document.getElementById('map'), {
+      zoom: 16,
+      center: artCoords,
+      gestureHandling: 'cooperative'
+    });
+  } 
+  
 
-  var marker = new google.maps.Marker({
-    position: artCoords,
-    map: map,
-    title: 'Art location'
-  });
+  if (!dialogMarker) {
+    dialogMarker = new google.maps.Marker({
+      position: artCoords,
+      map: dialogMap,
+      title: 'Art location'
+    });
+  } else {
+    dialogMarker.setPosition(artCoords);
+  }
 }
